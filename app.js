bd0633e09d9d0bbe88f2d09f0a5fab98c17e68f8
kuyole2000 <34594778+kuyole2000@users.noreply.github.com>
2022-06-05 16:38:07 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const {campgroundSchema} = require('./schemas.js')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError') 
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const res = require('express/lib/response');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -29,6 +31,15 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+        const { error } = campgroundSchema.validate(req.body);
+        if (error) {
+            const msg = error.details.map(el => el.message).join(',')
+            throw new ExpressError(msg, 400)
+        } else {
+            next();
+        }
+}
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -41,8 +52,8 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', catchAsync(async (req, res) => {
-    if(!req.body.campground) throw new ExpressError('Invalid Campground', 400)
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground', 400)
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -58,7 +69,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
